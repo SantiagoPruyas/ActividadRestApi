0787b5d835df692bb12411008b176c80892f8d21
SantiagoPruyas <spruyas@hotmail.com>
2023-10-23 08:47:21 -0300

    Ruteado de nuevas funciones

--- Rest-API/src/routes.js
+++ Rest-API/src/routes.js
@@ -9,3 +9,6 @@ router.delete('/persona', persona.delete);
 router.put('/persona', persona.update);
 router.get('/libros', libro.getAll);
 router.get('/libro', libro.getOne);
+router.post('/libro', libro.insertOne);
+router.delete('/libro', libro.deleteOne);
+router.put('/libro', libro.updateOne);
